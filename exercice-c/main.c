a1bcc59590edadfb030dc4642c5db1348969ad3d
AdrienBoiseau <adrien-97@hotmail.fr>
2015-10-04 15:05:34 +0200

    Initial Commit

---
+++ exercice-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  exercice-c
+//
+//  Created by Adrien Boiseau on 04/10/2015.
+//  Copyright © 2015 Adrien Boiseau. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
